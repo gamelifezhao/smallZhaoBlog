1cd30b7d7318522fc1d76d9cf7dd22cf4270f628
灰色大仙 <102589759+gamelifezhao@users.noreply.github.com>
2023-09-21 18:42:51 +0800

    Add files via upload

---
+++ my-app/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  server: {
+    port: 5000, // 指定要使用的端口号
+  },
+};
+
+module.exports = nextConfig;
---
+++ my-app/src/app/bolg-home/metadata/page.tsx
@@ -0,0 +1,9 @@
+import MetaLayout from "./layout";
+
+export default function Home() {
+  return (
+    <MetaLayout>
+      <h1>Hello, World!11</h1>
+    </MetaLayout>
+  );
+}
